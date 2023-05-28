4a90cb38cdb63536ac5fd0dec37d736ba184d89e
Frank Lai <frank810618@gmail.com>
2023-05-28 14:05:52 -0700

    Update update-script.py

--- update-script.py
+++ update-script.py
@@ -4,6 +4,7 @@ import os
 import sys
 import json
 import requests
+from azure.storage.blob import BlobServiceClient
 
 auth = Authorization(
   'data.wa.gov',
@@ -12,10 +13,10 @@ auth = Authorization(
 )
 
 socrata = Socrata(auth)
-view = socrata.views.lookup('hndz-vqe9')
+view = socrata.views.lookup('dc4r-6qu4')
 
 # These are the fields you want to update
-dataset_name = 'Asset_Type_by_AgencyV4_05-06-2023_a1a2'
+dataset_name = 'GAUserAndDevice_05-28-2023_4d9a'
 metadata = { 'name': dataset_name }
 action_type = 'update'
 permission = 'private'
@@ -28,21 +29,44 @@ revision_json = json.dumps({
     } 
 })
 
-with open('Asset_Type_by_AgencyV4.csv', 'rb') as my_file:
-  # Here, we're adding the revision_json to the .csv() method call
-  (revision, job) = socrata.using_config('Asset_Type_by_AgencyV4_05-06-2023_a1a2', view).csv(my_file, revision_json)
-  
-  job = job.wait_for_finish(progress = lambda job: print('Job progress:', job.attributes['status']))
-  
-  # Check if the job was successful
-  if job.attributes['status'] != 'successful':
-    sys.exit(1)
+# Connection string for Azure Blob Storage
+connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+
+# Blob container name
+container_name = os.getenv('CONTAINER_NAME')
+
+# Blob file name
+blob_name = 'GAUserAndDevice.csv'
+
+# Create a BlobServiceClient instance
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+# Get the BlobClient for the file
+blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+# Download the file locally
+local_file_name = 'GAUserAndDevice.csv'
+with open(local_file_name, 'wb') as file:
+    file.write(blob_client.download_blob().readall())
+
+with open(local_file_name, 'rb') as my_file:
+    # Here, we're adding the revision_json to the .csv() method call
+    (revision, job) = socrata.using_config(dataset_name, view).csv(my_file, revision_json)
+
+    job = job.wait_for_finish(progress=lambda job: print('Job progress:', job.attributes['status']))
+
+    # Check if the job was successful
+    if job.attributes['status'] != 'successful':
+        sys.exit(1)
+
+# Remove the local file
+os.remove(local_file_name)
 
 # This is the full path to the metadata API on the domain that you care about
 url = 'https://data.wa.gov/api/views/metadata/v1'
 
 # This is the unique identifier of the dataset you care about
-uid = 'hndz-vqe9'
+uid = 'dc4r-6qu4'
 
 # And this is your login information
 username = os.environ['MY_SOCRATA_USERNAME']
@@ -60,4 +84,3 @@ response = requests.patch('{}/{}'.format(url, uid),
                           data=json.dumps(data))
 
 print(response.json())
-print(response.headers)

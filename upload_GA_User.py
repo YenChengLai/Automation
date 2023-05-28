3ccf1042516daf068b887e42817a6af029e3d44c
Frank Lai <frank810618@gmail.com>
2023-05-28 14:02:33 -0700

    Create upload_GA_User.py

---
+++ upload_GA_User.py
@@ -0,0 +1,86 @@
+from socrata.authorization import Authorization
+from socrata import Socrata
+import os
+import sys
+import json
+import requests
+from azure.storage.blob import BlobServiceClient
+
+auth = Authorization(
+  'data.wa.gov',
+  os.environ['MY_SOCRATA_USERNAME'],
+  os.environ['MY_SOCRATA_PASSWORD']
+)
+
+socrata = Socrata(auth)
+view = socrata.views.lookup('dc4r-6qu4')
+
+# These are the fields you want to update
+dataset_name = 'GAUserAndDevice_05-28-2023_4d9a'
+metadata = { 'name': dataset_name }
+action_type = 'update'
+permission = 'private'
+
+revision_json = json.dumps({ 
+    'metadata': metadata, 
+    'action': { 
+        'type': action_type, 
+        'permission': permission 
+    } 
+})
+
+# Connection string for Azure Blob Storage
+connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+
+# Blob container name
+container_name = os.getenv('CONTAINER_NAME')
+
+# Blob file name
+blob_name = 'GAUserAndDevice.csv'
+
+# Create a BlobServiceClient instance
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+# Get the BlobClient for the file
+blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+# Download the file locally
+local_file_name = 'GAUserAndDevice.csv'
+with open(local_file_name, 'wb') as file:
+    file.write(blob_client.download_blob().readall())
+
+with open(local_file_name, 'rb') as my_file:
+    # Here, we're adding the revision_json to the .csv() method call
+    (revision, job) = socrata.using_config(dataset_name, view).csv(my_file, revision_json)
+
+    job = job.wait_for_finish(progress=lambda job: print('Job progress:', job.attributes['status']))
+
+    # Check if the job was successful
+    if job.attributes['status'] != 'successful':
+        sys.exit(1)
+
+# Remove the local file
+os.remove(local_file_name)
+
+# This is the full path to the metadata API on the domain that you care about
+url = 'https://data.wa.gov/api/views/metadata/v1'
+
+# This is the unique identifier of the dataset you care about
+uid = 'dc4r-6qu4'
+
+# And this is your login information
+username = os.environ['MY_SOCRATA_USERNAME']
+password = os.environ['MY_SOCRATA_PASSWORD']
+
+headers = {'Content-Type': 'application/json'}
+
+# These are the fields you want to update
+data = {'private': True}
+
+response = requests.patch('{}/{}'.format(url, uid),
+                          auth=(username, password),
+                          headers=headers,
+                          # the data has to be passed as a string
+                          data=json.dumps(data))
+
+print(response.json())
